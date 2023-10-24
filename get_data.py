f4883b892013ef2756f59b4fd116a896bb7b6a22
kamil <kamil271e@gmail.com>
2023-10-24 10:09:23 +0200

    [#3] Proper loss calculation

--- get_data.py
+++ get_data.py
@@ -3,25 +3,67 @@ import cdsapi
 c = cdsapi.Client()
 
 c.retrieve(
-    'reanalysis-era5-single-levels',
+    "reanalysis-era5-single-levels",
     {
-        'product_type': 'reanalysis',
-        'format': 'grib',
-        'variable': [
-            '2m_temperature', 'surface_pressure',
+        "product_type": "reanalysis",
+        "format": "grib",
+        "variable": [
+            "2m_temperature",
+            "surface_pressure",
         ],
-        'year': '2023',
-        'month': '10',
-        'day': [
-            '03', '04',
+        "year": "2023",
+        "month": [
+            "09",
+            "10",
         ],
-        'time': [
-            '00:00', '04:00', '08:00',
-            '12:00', '16:00', '20:00',
+        "day": [
+            "01",
+            "02",
+            "03",
+            "04",
+            "05",
+            "06",
+            "07",
+            "08",
+            "09",
+            "10",
+            "11",
+            "12",
+            "13",
+            "14",
+            "15",
+            "16",
+            "17",
+            "18",
+            "19",
+            "20",
+            "21",
+            "22",
+            "23",
+            "24",
+            "25",
+            "26",
+            "27",
+            "28",
+            "29",
+            "30",
         ],
-        'area': [
-            55, 14, 49,
+        "time": [
+            "00:00",
+            "04:00",
+            "08:00",
+            "12:00",
+            "16:00",
+            "20:00",
+        ],
+        "area": [
+            55,
+            14,
+            49,
             25,
         ],
     },
-    'data.grib')
+    "data.grib",
+)
+
+# ~ 30s

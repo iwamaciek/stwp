a403875f20841608c0b10a5da61f5f3cff72af47
kamil <kamil271e@gmail.com>
2023-10-30 22:23:28 +0100

    [#4] Adjust evaluation plots

--- linear_reg/regressor.py
+++ linear_reg/regressor.py
@@ -6,13 +6,16 @@ FEATURES_LIST = ["Temperature", "Pressure"]
 
 
 class Regressor:
-    def __init__(self, data_shape: tuple, regressor_type="linear_reg"):
+    def __init__(
+        self, data_shape: tuple, feature_list: list, regressor_type="linear_reg"
+    ):
         if regressor_type == "linear_reg":
             self.model = LinearRegression()
         else:
             print("Not implemented")
 
         _, self.input_state, self.latitude, self.longitude, self.features = data_shape
+        self.feature_list = feature_list
 
     def train(self, X_train, y_train):
         X = X_train.reshape(-1, self.input_state)
@@ -44,6 +47,8 @@ class Regressor:
         for i in range(limit):
             # y_test_sample = y_test[i].reshape(self.longitude * self.latitude, self.features)
             # y_hat_sample = y_hat[i].reshape(self.longitude * self.latitude, self.features)
+            fig, ax = plt.subplots(self.features, 2, figsize=(8, 5))
+
             for j in range(self.features):
                 # y_t = y_test_sample[:, j]
                 # y_h = y_hat_sample[:, j]
@@ -51,14 +56,13 @@ class Regressor:
                 # r2 = r2_score(y_t, y_h)
 
                 if verbose:
-                    fig, ax = plt.subplots(1, 2, figsize=(12, 5))
-                    plt.suptitle(FEATURES_LIST[j])
-                    ax[0].imshow(y_hat[i, :, :, j], cmap=plt.cm.coolwarm)
-                    ax[0].set_title("Predicted")
-                    ax[0].axis("off")
-                    ax[1].imshow(y_test[i, :, :, j], cmap=plt.cm.coolwarm)
-                    ax[1].set_title("Actual")
-                    ax[1].axis("off")
-                    plt.show()
+                    ax[j, 0].imshow(y_hat[i, :, :, j], cmap=plt.cm.coolwarm)
+                    ax[j, 0].set_title(f"Predicted [{self.feature_list[j]}]")
+                    ax[j, 0].axis("off")
+                    ax[j, 1].imshow(y_test[i, :, :, j], cmap=plt.cm.coolwarm)
+                    ax[j, 1].set_title(f"Actual [{self.feature_list[j]}]")
+                    ax[j, 1].axis("off")
 
-                # print(f"MSE: {mse}; R2: {r2}")
+            plt.show()
+
+            # print(f"MSE: {mse}; R2: {r2}")

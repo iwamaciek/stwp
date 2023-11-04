502c74ccf9d080fac2901377df2c474d2d45db66
kamil <kamil271e@gmail.com>
2023-11-04 12:21:23 +0100

    [#4] Error plots

--- linear_reg/regressor.py
+++ linear_reg/regressor.py
@@ -30,7 +30,6 @@ class Regressor:
         for i in range(self.features):
             yi = y_train[:, :, :, 0, i].reshape(-1, 1)
             self.models[i].fit(X, yi)
-        return X, yi
 
     def evaluate(self, y_hat, y_test):
         rmse_features = []
@@ -40,6 +39,47 @@ class Regressor:
             err = round(np.sqrt(mean_squared_error(y_hat_i, y_test_i)), 3)
             rmse_features.append(err)
         return rmse_features
+
+    def plot_predictions(self, y_hat, y_test, max_samples):
+        for i in range(max_samples):
+            y_test_sample, y_hat_sample = y_test[i], y_hat[i]
+            fig, ax = plt.subplots(
+                self.features, 3 * self.fh, figsize=(10 * self.fh, 3 * self.features)
+            )
+
+            for j in range(self.features):
+                cur_feature = self.feature_list[j]
+                y_test_sample_feature_j = y_test_sample[:, :, :, j].reshape(-1, 1)
+                y_hat_sample_feature_j = y_hat_sample[:, :, :, j].reshape(-1, 1)
+                mse = mean_squared_error(
+                    y_test_sample_feature_j, y_hat_sample_feature_j
+                )
+                rmse = np.sqrt(mse)
+                print(f"RMSE {cur_feature}: {round(rmse,5)}")
+
+                for k in range(3 * self.fh):
+                    ts = k // 3
+                    if k % 3 == 0:
+                        title = rf"$X_{{{cur_feature},t+{ts+1}}}$"
+                        value = y_test[i, :, :, ts, j]
+                        cmap = plt.cm.coolwarm
+                    elif k % 3 == 1:
+                        title = rf"$\hat{{X}}_{{{cur_feature},t+{ts+1}}}$"
+                        value = y_hat[i, :, :, ts, j]
+                        cmap = plt.cm.coolwarm
+                    else:
+                        title = (
+                            rf"$f_{{RMSE}}(X - \hat{{X}})_{{{cur_feature},t+{ts+1}}}$"
+                        )
+                        value = np.sqrt(
+                            (y_test[i, :, :, ts, j] - y_hat[i, :, :, ts, j]) ** 2
+                        )
+                        cmap = "binary"
+                    pl = ax[j, k].imshow(value, cmap=cmap)
+                    ax[j, k].set_title(title)
+                    ax[j, k].axis("off")
+                    _ = fig.colorbar(pl, ax=ax[j, k], fraction=0.15)
+            plt.show()
 
     def predict_and_evaluate(self, X_test, y_test, max_samples=5):
         X = X_test.reshape(-1, self.input_state * self.features)
@@ -56,45 +96,15 @@ class Regressor:
         else:
             y_hat = self.predict_autoreg(X_test, y_test)
 
-        rmse_features = self.evaluate(y_hat, y_test)
-
-        for i in range(max_samples):
-            y_test_sample, y_hat_sample = y_test[i], y_hat[i]
-            fig, ax = plt.subplots(
-                self.features, 2 * self.fh, figsize=(8 * self.fh, 3 * self.features)
-            )
-
-            for j in range(self.features):
-                cur_feature = self.feature_list[j]
-                y_test_sample_feature_j = y_test_sample[:, :, :, j].reshape(-1, 1)
-                y_hat_sample_feature_j = y_hat_sample[:, :, :, j].reshape(-1, 1)
-                mse = mean_squared_error(
-                    y_test_sample_feature_j, y_hat_sample_feature_j
-                )
-                rmse = np.sqrt(mse)
-                print(f"RMSE {cur_feature}: {round(rmse,3)}")
-
-                for k in range(2 * self.fh):
-                    ts = k // 2
-                    if k % 2 == 0:
-                        title = rf"$X_{{{cur_feature},t+{ts+1}}}$"
-                        value = y_test[i, :, :, ts, j]
-                    else:
-                        title = rf"$\hat{{X}}_{{{cur_feature},t+{ts+1}}}$"
-                        value = y_hat[i, :, :, ts, j]
-                    pl = ax[j, k].imshow(value, cmap=plt.cm.coolwarm)
-                    ax[j, k].set_title(title)
-                    ax[j, k].axis("off")
-                    _ = fig.colorbar(pl, ax=ax[j, k], fraction=0.15)
-            plt.show()
-
+        self.plot_predictions(y_hat, y_test, max_samples=max_samples)
+        eval_scores = self.evaluate(y_hat, y_test)
         print("=======================================")
         print("Evaluation metrics for entire test set:")
         print("=======================================")
         for i in range(self.features):
-            print(f"RMSE {self.feature_list[i]}: {rmse_features[i]}")
+            print(f"RMSE {self.feature_list[i]}: {eval_scores[i]}")
 
-        return X, y_hat
+        return y_hat
 
     def predict_autoreg(self, X_test, y_test):
         """
@@ -126,7 +136,7 @@ class Regressor:
                     .reshape(1, self.latitude, self.longitude)
                 )
                 for k in range(self.fh - 1):
-                    # TODO fix concatenation
+                    # TODO fix concatenation, is it even possible for distinct models?
                     Xij = np.concatenate(
                         (Xi[:, :, k + 1 :, :], y_hat_ij[:, :, : k + 1]), axis=0
                     )
@@ -137,4 +147,5 @@ class Regressor:
                         .reshape(1, self.latitude, self.longitude)
                     )
                 y_hat[i, :, :, :, j] = y_hat_ij
+
         return y_hat

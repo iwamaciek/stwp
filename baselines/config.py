0fef2844933578f74f10aeda42b435cea58a7bb2
kamil <kamil271e@gmail.com>
2023-12-05 12:16:41 +0100

    [#3] Test AdamW

--- baselines/config.py
+++ baselines/config.py
@@ -2,7 +2,7 @@ import torch
 
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 TRAIN_RATIO = 0.5
-BATCH_SIZE = 32
+BATCH_SIZE = 4
 FH = 1
 INPUT_SIZE = 3
 DATA_PATH = "../data2021-2022.grib"

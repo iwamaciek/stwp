ca8ba03435092e89496d86b8e7a50a560d2d8149
kamil <kamil271e@gmail.com>
2023-12-28 16:39:27 +0100

    [#21] Fix config class bug; add scaler_type field

--- models/config.py
+++ models/config.py
@@ -4,10 +4,11 @@ import torch
 class Config:
     DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     TRAIN_RATIO = 1 / 3
-    BATCH_SIZE = 1
+    BATCH_SIZE = 8
     FH = 1
     INPUT_SIZE = 5
     R = 2
+    SCALER_TYPE = "robust"
     DATA_PATH = "../data/data2019-2021.grib"
     RANDOM_STATE = 42
     INPUT_DIMS = (32, 48)
